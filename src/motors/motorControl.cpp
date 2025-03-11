da289e6f2511e5bd963b53cbb4def5ce475de3c2
Coleman Reed <ctreed2003@gmail.com>
2025-03-11 16:38:59 -0400

    Motor and bolt detector code intial commit

--- src/motors/motorControl.cpp
+++ src/motors/motorControl.cpp
@@ -6,11 +6,66 @@ Code for motor controls
 
 */
 
+#include "motorControl.h"
+
 //motor controller
 static const int MTR_STEP_PIN = 13;
 static const int MTR_DIR_PIN = 14;
 static const int MTR_ENA_PIN = 15;
+static const int BRK_BEAM_PIN = 38;
 
-void initMotor();
-void motorClockwise(int onTime, int dutyCycle);
-void motorCounterClockwise(int onTime, int dutyCycle);
+unsigned long startTime;
+unsigned long stopTime;
+
+void initMotor() {
+    pinMode(MTR_STEP_PIN, OUTPUT);
+    pinMode(MTR_DIR_PIN, OUTPUT);
+    pinMode(MTR_ENA_PIN, OUTPUT);
+
+    digitalWrite(MTR_ENA_PIN, LOW);
+
+    // high is counterclockwise, low is clockwise
+    digitalWrite(MTR_DIR_PIN, HIGH);
+
+}
+
+void initBoltDetector() {
+    pinMode(BRK_BEAM_PIN, INPUT);
+}
+
+int readBoltDetector() {
+    return digitalRead(BRK_BEAM_PIN);
+}
+
+// onTime in ms
+void motorClockwise(int onTime) {
+    digitalWrite(MTR_DIR_PIN, LOW);
+
+    startTime = millis();
+    stopTime = startTime;
+
+    while((stopTime - startTime) <= onTime) {
+        digitalWrite(MTR_STEP_PIN, HIGH);
+        delay(1);
+        digitalWrite(MTR_STEP_PIN, LOW);
+        delay(1);
+
+        stopTime = millis();
+    }
+}
+
+void motorCounterClockwise(int onTime) {
+    digitalWrite(MTR_DIR_PIN, HIGH);
+
+    startTime = millis();
+    stopTime = startTime;
+
+    while((stopTime - startTime) <= onTime) {
+        digitalWrite(MTR_STEP_PIN, HIGH);
+        delay(1);
+        digitalWrite(MTR_STEP_PIN, LOW);
+        delay(1);
+        
+        stopTime = millis();
+    }
+}
--- src/motors/motorControl.h
+++ src/motors/motorControl.h
@@ -1,3 +1,6 @@
+#include <Arduino.h>
 void initMotor();
+void initBoltDetector();
+int readBoltDetector();
 void motorClockwise(int onTime, int dutyCycle);
 void motorCounterClockwise(int onTime, int dutyCycle);
