583b05ea63d213c1a6339181dec4441fc72699b2
Coleman Reed <ctreed2003@gmail.com>
2025-03-11 16:36:37 -0400

    Solenoid code initial commit

--- src/solenoids/solenoids.h
+++ src/solenoids/solenoids.h
@@ -2,3 +2,4 @@ void initSolenoids();
 void extendSolenoid0(); // solenoid for keypad button '0'
 void extendSolenoid2(); // solenoid for keypad button '0'
 void extendSolenoid5(); // solenoid for keypad button '0'
+void solenoidsEnterCode();
